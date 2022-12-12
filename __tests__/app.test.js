1e87d88ffa47f5fd2bd85ed72bcb31ea5ed33ad4
jxnbox <janr3p@gmail.com>
2022-12-12 13:31:08 +0000

    updated README

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const db = require('../db/connection');
 const seed = require('../db/seeds/seed');
-const testData = require('../db/data/test-data');
+const testTopicData = require('../db/data/test-data/topics');
 const { app } = require("../app");
 
 afterAll(() => db.end());
-beforeEach( () => seed(testData));
+beforeEach( () => seed(testTopicData));
